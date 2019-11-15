a9cd6440156044eacab93b3cb67752a1125bd7aa
Tyler-Blood <TylerLBlood@gmail.com>
2019-11-15 15:53:13 -0500

    added copyright and versioning

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -3,6 +3,7 @@
       <h4>this is the footer</h4>
       <p>we place redundant information here</p>
       <div id="footerTagLine">I am 7 inches left of normal</div>
+      <div class="copyright">iop project V.<?php echo VERSION; ?> | &copy; <?php echo date("Y"); ?> | Tyler Blood</div>
     </footer>
 
   </body>
